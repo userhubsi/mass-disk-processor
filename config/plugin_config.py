4aa193849fc4d1e5f7c43fa012ecf03971d0bf6d
userhubsi <userhubsi@protonmail.com>
2026-02-04 19:38:25 +0100

    Fix config and registry

--- config/plugin_config.py
+++ config/plugin_config.py
@@ -1,36 +1,36 @@
-from mdp_plugins import bulk_extractor
-
-
 enabled_plugins = [
-    # # General
-    # "disk_size",
-    # "no_partitions",
-    # "no_partition_types",
-    # "no_files",
-    # "num_user_files",
-    # "file_types",
-    # "file_size_stats",
-    # "fs_lifespan",
-    # "operating_system_detect",
-    # # Browser history
-    # "firefox_history",
-    # "chrome_history",
-    # "edge_history",
-    # # Windows-specific
-    # "win_lifespan",
-    # "win_version",
-    # "win_user_info",
-    # "win_computer_and_user_names",
-    # "win_screen_resolution",
-    # "win_evt_logs",
-    # "win_apps",
-    # "win_browsers",
-    # "win_num_user_lnk_files",
-    # "win_num_prefetch_files",
-    # "win_num_usbs",
-    # "win_num_wifi_connections",
-    # # External
-    # # # "external_program_demo",
-    # # "plaso"
-    "bulk_extractor",
+    # General
+    "disk_size",
+    "no_partitions",
+    "no_partition_types",
+    "no_files",
+    "num_user_files",
+    "file_types",
+    "file_size_stats",
+    "fs_lifespan",
+    "operating_system_detect",
+
+    # Browser history
+    "firefox_history",
+    "chrome_history",
+    "edge_history",
+
+    # Windows-specific
+    "win_lifespan",
+    "win_version",
+    "win_user_info",
+    "win_computer_and_user_names",
+    "win_screen_resolution",
+    "win_evt_logs",
+    "win_apps",
+    "win_browsers",
+    "win_num_user_lnk_files",
+    "win_num_prefetch_files",
+    "win_num_usbs",
+    "win_num_wifi_connections",
+
+    # External
+    # # "external_program_demo",
+    # "plaso",
+    # "bulk_extractor"
 ]
